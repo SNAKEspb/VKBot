68fd605f53de499b80d339f5c9efffb0c63a216c
dandreev <grafity88@live.com>
2019-03-25 00:28:24 +0300

    message to meme logic

--- VKBot/VKBot/VkontakteBot/Models/TextMessageHandler.cs
+++ VKBot/VKBot/VkontakteBot/Models/TextMessageHandler.cs
@@ -8,31 +8,67 @@ namespace VKBot.VkontakteBot.Models
 {
     public class TextMessageHandler : IUpdatesHandler<IIncomingMessage>  
     {
-        static string[] messageTypes = new[] { "message_new", "message_reply" };
+        static string[] _messageTypes = new[] { "message_new", "message_reply" };
+        static string[] _memePhrases = new[] { "!", "говорит:" };
         static string[] userIds = new[]
         {
-            //"212515973",//vitya
-            "1556462"//me
+            "212515973",//vitya
+            //"1556462"//me
         };
         public bool CanHandle(IIncomingMessage message, IVityaBot bot)
         {
-            return messageTypes.Contains(message.MessageType.ToLowerInvariant())
-                && userIds.Contains(message.from_id)
-                && !string.IsNullOrEmpty(message.text);
+            return _messageTypes.Contains(message.MessageType.ToLowerInvariant())
+                && !string.IsNullOrWhiteSpace(message.text)
+                && (userIds.Contains(message.from_id) || checkCommand(message.text));
         }
         public async Task<HandlerResult> HandleAsync(IIncomingMessage message, IVityaBot bot)
+        {         
+            var text = tryParseCommand(message);
+
+            await processMemeAsync(message, bot, text);
+            return new HandlerResult() { message = "ok" };
+        }
+        //todo: command handler
+        private bool checkCommand(string text)
         {
-            var memeUrl = await bot.imgFlipCaptionImage(message.text);
+            return _memePhrases.Any(t => text.ToLowerInvariant().StartsWith(t) && text.Length > t.Length);
+        }
+
+        private string tryParseCommand(IIncomingMessage message)
+        {
+            //todo: regexp or any command parser
+            if (userIds.Contains(message.from_id))
+            {
+                return message.text;
+            }
+            foreach (var memeCommand in _memePhrases.Where(t => message.text.ToLowerInvariant().StartsWith(t)))
+            {
+                int index = message.text.ToLowerInvariant().IndexOf(memeCommand);
+                if (index != -1)
+                {
+                    var result = message.text.Substring(index + memeCommand.Length);
+                    if (!string.IsNullOrWhiteSpace(result))
+                    {
+                        return result;
+                    }
+                } 
+            }
+            throw new ArgumentException($"Message text is empty");
+        }
+
+        //todo: move to bot
+        private async Task processMemeAsync(IIncomingMessage message, IVityaBot bot, string text)
+        {
+            var memeUrl = await bot.imgFlipCaptionImage(text);
             var photoId = await bot.savePhotoByUrl(memeUrl, message.peer_id);
             var outgoingMessage = new OutgoingMessage()
             {
                 peer_id = message.peer_id,
-                message = string.Format("Все говорят {0}, а ты пошел на хуй", message.text),
+                message = text,
                 attachment = photoId,
                 //group_id = message.
             };
             await bot.SendMessageAsync(outgoingMessage);
-            return new HandlerResult() { message = "ok" };
         }
     }
 }
