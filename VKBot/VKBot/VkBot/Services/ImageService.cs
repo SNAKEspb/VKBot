b34218a9d43504da2a4b23e3233d1592f65c3151
dandreev <grafity88@live.com>
2022-04-06 01:03:15 +0300

    remove system.drawing

--- VKBot/VKBot/VkBot/Services/ImageService.cs
+++ VKBot/VKBot/VkBot/Services/ImageService.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Drawing.Drawing2D;
 using System.IO;
-using System.Drawing.Text;
+
 
 namespace VKBot.VkontakteBot.Services
 {
@@ -30,26 +27,32 @@ namespace VKBot.VkontakteBot.Services
 
         public byte[] addTextToImage(byte[] imageData, string text)
         {
-            using (var inputStream = new MemoryStream(imageData))
-            using (var bitMap = new Bitmap(inputStream))
-            using (var graphic = Graphics.FromImage(bitMap))
-            //using (var font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Regular))
-            using (var font = new Font("Calibri", (int)getSize(bitMap, text), FontStyle.Regular))
-            {
+            //using (var image = Image.Load(inputStream, out format))
+            //{
+            //    image.Mutate(c => c.Resize(30, 30));
+            //    image.Save(outputStream, format);
+            //}
 
-                graphic.InterpolationMode = InterpolationMode.High;
-                graphic.SmoothingMode = SmoothingMode.HighQuality;
-                graphic.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-                graphic.CompositingQuality = CompositingQuality.HighQuality;
+            //using (var inputStream = new MemoryStream(imageData))
+            //using (var bitMap = new Bitmap(inputStream))
+            //using (var graphic = Graphics.FromImage(bitMap))
+            ////using (var font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Regular))
+            //using (var font = new Font("Calibri", (int)getSize(bitMap, text), FontStyle.Regular))
+            //{
 
-                drawText(graphic, text, font);
-                //bitMap.Save("d:\\output.jpg", ImageFormat.Jpeg);
-                using (var outputStream = new MemoryStream())
-                {
-                    bitMap.Save(outputStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-                    return outputStream.ToArray();
-                }
-            }
+            //    graphic.InterpolationMode = InterpolationMode.High;
+            //    graphic.SmoothingMode = SmoothingMode.HighQuality;
+            //    graphic.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+            //    graphic.CompositingQuality = CompositingQuality.HighQuality;
+
+            //    drawText(graphic, text, font);
+            //    //bitMap.Save("d:\\output.jpg", ImageFormat.Jpeg);
+            //    using (var outputStream = new MemoryStream())
+            //    {
+            //        bitMap.Save(outputStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            //        return outputStream.ToArray();
+            //    }
+            //}
             return new byte[0];
         }
 
@@ -69,53 +72,53 @@ namespace VKBot.VkontakteBot.Services
         //    }
         //}
 
-        public void drawText(Graphics graphics, string text, Font font) {
+        //public void drawText(Graphics graphics, string text, Font font) {
 
-            var OutlineForeColor = Color.White;
-            var OutlineWidth = 2;
-            var ForeColor = Color.Black;
+        //    var OutlineForeColor = Color.White;
+        //    var OutlineWidth = 2;
+        //    var ForeColor = Color.Black;
 
-            using (GraphicsPath graphicsPath = new GraphicsPath())
-            using (Pen outline = new Pen(OutlineForeColor, OutlineWidth) { LineJoin = LineJoin.Round })
-            using (StringFormat stringFormat = new StringFormat())
-            using (Brush foreBrush = new SolidBrush(ForeColor))
-            {
+        //    using (GraphicsPath graphicsPath = new GraphicsPath())
+        //    using (Pen outline = new Pen(OutlineForeColor, OutlineWidth) { LineJoin = LineJoin.Round })
+        //    using (StringFormat stringFormat = new StringFormat())
+        //    using (Brush foreBrush = new SolidBrush(ForeColor))
+        //    {
 
-                var point = getPoint(graphics, text, font);
+        //        var point = getPoint(graphics, text, font);
 
-                graphicsPath.AddString(text, font.FontFamily, (int)font.Style, graphics.DpiY * font.Size / 72, point, stringFormat);
-                //graphic.ScaleTransform(1.3f, 1.35f);
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.DrawPath(outline, graphicsPath);
-                graphics.FillPath(foreBrush, graphicsPath);
-            }
-        }
+        //        graphicsPath.AddString(text, font.FontFamily, (int)font.Style, graphics.DpiY * font.Size / 72, point, stringFormat);
+        //        //graphic.ScaleTransform(1.3f, 1.35f);
+        //        graphics.SmoothingMode = SmoothingMode.HighQuality;
+        //        graphics.DrawPath(outline, graphicsPath);
+        //        graphics.FillPath(foreBrush, graphicsPath);
+        //    }
+        //}
 
-        public decimal getSize(Image img, string text)
-        {
-            return img.Width / text.Length;
-        }
+        //public decimal getSize(Image img, string text)
+        //{
+        //    return img.Width / text.Length;
+        //}
 
-        public Point getPoint(Graphics graphics, string text, Font font)
-        {
-            var measureResult = graphics.MeasureString(text, font);
+        //public Point getPoint(Graphics graphics, string text, Font font)
+        //{
+        //    var measureResult = graphics.MeasureString(text, font);
 
-            var minWidth = graphics.VisibleClipBounds.Width * 0.05;
-            var maxWidth = calculateMax(graphics.VisibleClipBounds.Width, minWidth, measureResult.Width);
+        //    var minWidth = graphics.VisibleClipBounds.Width * 0.05;
+        //    var maxWidth = calculateMax(graphics.VisibleClipBounds.Width, minWidth, measureResult.Width);
 
-            var minHeight = graphics.VisibleClipBounds.Height * 0.05;
-            var maxHeight = calculateMax(graphics.VisibleClipBounds.Height, minHeight, measureResult.Height);
+        //    var minHeight = graphics.VisibleClipBounds.Height * 0.05;
+        //    var maxHeight = calculateMax(graphics.VisibleClipBounds.Height, minHeight, measureResult.Height);
 
-            Random rand = new Random();
-            var pointX = rand.Next((int)minWidth, (int)maxWidth);
-            var pointY = rand.Next((int)minHeight, (int)maxHeight);
-            return new Point(pointX, pointY);
-        }
+        //    Random rand = new Random();
+        //    var pointX = rand.Next((int)minWidth, (int)maxWidth);
+        //    var pointY = rand.Next((int)minHeight, (int)maxHeight);
+        //    return new Point(pointX, pointY);
+        //}
 
-        public double calculateMax(double total, double min, double size)
-        {
-            return total - min - size;
-        }
+        //public double calculateMax(double total, double min, double size)
+        //{
+        //    return total - min - size;
+        //}
 
 
         //public static byte[] imageToByte(Image img)
