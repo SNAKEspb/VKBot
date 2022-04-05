80914d5ce32af97bb887d6c5365d2f3ddb5274dd
dandreev <grafity88@live.com>
2022-04-05 17:08:42 +0300

    migrate to netcoreapp3.1

--- VKBot/VKBot/Startup.cs
+++ VKBot/VKBot/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -29,7 +30,7 @@ namespace VKBot
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -42,12 +43,14 @@ namespace VKBot
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
-            app.UseMvc(routes =>
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "VkAuth",
-                    template: "{controller=VkUserApi}/{action=Index}");
+                endpoints.MapControllers();
             });
         }
     }
